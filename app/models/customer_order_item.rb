526d0ff4f4013c261027542f5c49b92646333ebb
Cameron Roe <cameron@cameronbroe.com>
2020-09-27 21:11:31 -0500

    Validate a customer order item

--- app/models/customer_order_item.rb
+++ app/models/customer_order_item.rb
@@ -1,4 +1,6 @@
 class CustomerOrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer_order
+
+  validates_numericality_of :quantity
 end
--- spec/models/customer_order_item_spec.rb
+++ spec/models/customer_order_item_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe CustomerOrderItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(
+        item: Item.new(description: "Crowbar", price: 19.99),
+        customer_order: CustomerOrder.new(
+          customer: Customer.new(first_name: "John", last_name: "Doe"),
+          merchant: Merchant.new(name: "Merchant Co", address: "123 Main St")
+        ),
+        quantity: 1
+    )
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without an item" do
+    subject.item = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a quantity" do
+    subject.quantity = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a quantity with numericality" do
+    should validate_numericality_of(:quantity)
+  end
 end
